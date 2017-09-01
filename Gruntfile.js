34eaccd926c74337cce4df391b3751e3064dde80
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-09-01 23:06:33 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: ['js/*.js']
+		},
+		watch: {
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['jshint'],
+				options: {
+					spawn: false,
+				}
+			}
+		}
+	});
+
+		// Load the plugins tasks
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	
+		// Default task(s).
+	grunt.registerTask('default', ['jshint', 'watch'])
+	
+};
